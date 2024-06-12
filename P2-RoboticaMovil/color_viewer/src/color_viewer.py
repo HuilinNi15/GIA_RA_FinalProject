1b6e291bd0cf2f1408b9154c968c14efebe6fab0
S3RXxX <sergiguimeraroig@gmail.com>
2024-06-12 18:50:01 +0200

    ara executa, falta ajustar paràmetres i tot el relatiu a altres nodes

--- P2-RoboticaMovil/color_viewer/src/color_viewer.py
+++ P2-RoboticaMovil/color_viewer/src/color_viewer.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 import rospy
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan, Image
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseResult, MoveBaseFeedback
-import actionlib
+import roslib
 import numpy as np
 import cv2
-import math
+import sys
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+# from geometry_msgs.msg import Twist
+# from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseResult, MoveBaseFeedback
+# import actionlib
+
 
 class color_viewer:
     def __init__(self) -> None:
         rospy.loginfo("Started color viewer node")
 
-        self.colors = {"red":np.array([0, 0, 255]), "green":np.array([0, 255, 0]), "blue": np.array([255, 0, 0])}
+        self.colors = {"red":np.array([0, 0, 255]), "green":np.array([0, 255, 0]), "blue": np.array([255, 0, 0])} #bgr en cv2
         self.color_threshold = 100
         self.pixel_count_threshold = 100 ####################33 ajustar depen el tamany de la imatge
-
-        self.sub = rospy.Subscriber("/camera/image", Image, callback=self.camera_callback)  # canviar de LaserScan a Image? (mirar com es diu)
+        self.step = 10 ################## ajustar depen el tamany de la imatge
+        self.bridge_object = CvBridge()
+        self.sub = rospy.Subscriber("/camera/rgb/image_raw", Image, callback=self.camera_callback)  # canviar de LaserScan a Image? (mirar com es diu)
    
-    def color_distance(c1, c2):
+    def color_distance(self, c1, c2):
         return np.sqrt(np.sum((c1 - c2) ** 2))
     
     def check_1_color(self, color_str, pixel):
@@ -26,18 +30,28 @@ class color_viewer:
 
     def check_colors(self):
         c = {color_str: 0 for color_str in self.colors}
-        for row in self.image:
+        sampled_image = self.image[::self.step, ::self.step]
+        for row in sampled_image:
             for pixel in row:
                 for color_str in self.colors:
-                    self.check_1_color(color_str, pixel)
+                    c[color_str] += self.check_1_color(color_str, pixel)
+        m = max(c, key=c.get)
+        self.color = m if c[m]*10 > self.pixel_count_threshold else None # si utilitzem un threshold posat a ma
+        # self.color = m if c[m]*10 > self.image.size//2 else None
 
     def camera_callback(self, messages):
-        self.image = messages.image
+        try:
+            self.image = self.bridge_object.imgmsg_to_cv2(messages, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            print(e)
         # mirem si hi ha algun color que s'assembla
         self.check_colors()
         if self.color:
-            rospy.loginfo(f"Found color {self.color} UwU")
-        self.update_movement()
+            rospy.loginfo(f"Found color {self.color} OvO")
+        else:
+            rospy.loginfo("No signal color found")
+        
+        # aqui publicar (o cridar un metode q publiqui) quan ja vagi (missatge personalitzat)
 
 if __name__== "__main__":
     np.random.seed(373)
