0fca8fa3a9d05f5ace08f42f5ff8e043c7724321
SandraJimenez231203 <sandra.jimenez.vargas@estudiantat.upc.edu>
2024-06-16 12:57:29 +0200

    ya funciona

--- P2-RoboticaMovil/color_mover/src/color_mover.py
+++ P2-RoboticaMovil/color_mover/src/color_mover.py
@@ -12,19 +12,19 @@ from std_srvs.srv import Empty
 class color_mover:
     def __init__(self) -> None:
         rospy.loginfo("Started color mover node")
-        self.map_colors = {0: None, 1: "red", 2: "green", 3: "blue", 4: "yellow"} ## es pot canviar per una llista
+        self.map_colors = {0: None, 1: "red", 2: "green", 3: "blue", 4: "yellow"}
         
-        self.sub = rospy.Subscriber('integer_topic', Int8,callback=self.color_callback)
-        
+        self.sub = rospy.Subscriber('integer_topic', Int8, callback=self.color_callback)
         self.move_base_client = actionlib.SimpleActionClient("/move_base", MoveBaseAction)
-        
         self.move_base_client.wait_for_server() 
-
         rospy.loginfo("move_base loaded")
 
         self.goal = MoveBaseGoal()
         self.goal.target_pose.pose.orientation.w = 1
         self.l = 0.3
+        self.color_detected = False  # Make sure this is defined before any callback can occur
+        self.color = None  # Initialize self.color as well
+
         rospy.loginfo("Clearing space")
         self.clear_unknown_space()
         rospy.loginfo("Space cleared")
@@ -49,8 +49,13 @@ class color_mover:
         """ 
         rospy.loginfo(f"goal {self.goal}")
         self.goal.target_pose.header.frame_id = "base_link"
-        self.goal.target_pose.pose.position.x = x
-        self.goal.target_pose.pose.position.y = y
+        if self.color_detected:
+            self.goal.target_pose.pose.position.x = x
+            self.goal.target_pose.pose.position.y = y
+        else:
+            self.goal.target_pose.pose.position.x = self.l
+            self.goal.target_pose.pose.position.y = 0
+            
         self.move_base_client.send_goal(self.goal)
         if wait: # esperar o no,  aquesta es la questio
             self.move_base_client.wait_for_result()
@@ -58,33 +63,36 @@ class color_mover:
     def color_callback(self, messages):
         color_int = messages.data
         self.color = self.map_colors[color_int]
-        # rospy.loginfo(f"Recieved color {self.color}")
-        rospy.loginfo(self.l)
-        if not self.color:
-        
-            # si no hi ha res tirar endavant
-            self.move_to_goal(0, self.l, wait=True)
-        elif self.color == "red": ## parar 3 segons
-            rospy.sleep(3)
-            self.move_to_goal(0, self.l, wait=True)
-        elif self.color == "green":  ## girar esquerra (n dist a esq)
-            self.move_to_goal(self.l, 0, wait=True)
-        elif self.color == "blue":  ## girar dreta (n dist a dreta)
-            self.move_to_goal(-self.l, 0, wait=True)
-        elif self.color == "yellow":
-            """self.move_arm()
-            self.move_to_goal(0, self.l, wait=True)
-            self.move_arm"""
-            rospy.loginfo("NOT IMPLEMENTED YET") # aixecar el braç
-        else:
-            raise ValueError('NOT IMPLEMENTED')
+        rospy.loginfo(f"Received color {self.color}")
+        rospy.loginfo(f"l value: {self.l}")
 
+        if self.color is None:
+            # If no color is detected, move forward
+            self.move_to_goal(0, self.l, wait=False)
+            self.color_detected = False
+        else:
+            # Color is detected, process according to color
+            self.color_detected = True
+            if self.color == "red":
+                rospy.sleep(3)
+                self.move_to_goal(0, self.l, wait=True)
+            elif self.color == "green":
+                self.move_to_goal(self.l, 0, wait=True)
+            elif self.color == "blue":
+                self.move_to_goal(-self.l, 0, wait=True)
+            elif self.color == "yellow":
+                rospy.loginfo("NOT IMPLEMENTED YET")  # Placeholder for arm movement
+            else:
+                rospy.logerr(f"Unsupported color action for {self.color}")
 
 if __name__== "__main__":
-    np.random.seed(373)
     rospy.init_node("color_mover_node")
-    color_mover()
+    cm = color_mover()
     try:
         rospy.spin()
     except rospy.ROSInterruptException:
         print("ROS Interrupt Exception")
+        
+        
+        
+        
