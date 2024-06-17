8d0509e9d928b1136ab301fb10b76c9b1aafd6d8
HuilinNi <104710729+HuilinNi15@users.noreply.github.com>
2024-06-17 12:09:21 +0200

    Add files via upload

---
+++ P2-RoboticaMovil/color_viewer.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+import rospy
+import roslib
+import numpy as np
+import cv2
+import sys
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from std_msgs.msg import Int8
+# from geometry_msgs.msg import Twist
+# from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseResult, MoveBaseFeedback
+# import actionlib
+
+
+class color_viewer:
+    def __init__(self) -> None:
+        rospy.loginfo("Started color viewer node")
+
+        self.colors = {"red":np.array([95, 55, 226]), "green":np.array([50, 181, 91]), "blue": np.array([250, 0, 0]), "yellow": np.array([36, 161, 183])} #bgr en cv2
+        self.map_colors = {None: 0, "red": 1, "green": 2, "blue": 3, "yellow": 4}
+        self.color_threshold = 75
+        self.pixel_count_threshold = 4000# 100000 # 1080 * 1920 // 10 ####################33 ajustar depen el tamany de la imatge
+        self.step = 1 
+        self.bridge_object = CvBridge()
+        
+        # colors for HSV
+        # definir a ma###############################
+        self.colors_hsv = {}
+        '''for color_str in self.colors:
+            col = np.uint8([[self.colors[color_str]]])
+            hsv_col = cv2.cvtColor(col,cv2.COLOR_BGR2HSV)
+            upper_lim = hsv_col + 5
+            lower_lim = hsv_col - 5
+            self.colors_hsv[color_str] = (lower_lim, upper_lim)'''
+        self.sub = rospy.Subscriber("/camera/image", Image, callback=self.camera_callback)
+        # self.sub = rospy.Subscriber("/camera/rgb/image_raw", Image, callback=self.camera_callback)
+
+        self.pub = rospy.Publisher('integer_topic', Int8, queue_size=1)
+   
+    ###################################
+    ### methods for BGR colors#########
+    ###################################
+    def color_distance(self, c1, c2):
+    	# return np.abs(np.sum((c1 - c2)))  # funciona millor al quadrat pq si un R, G, B és diferent la dist aumenta
+        return np.sqrt(np.sum((c1 - c2) ** 2))
+    
+    def check_1_color(self, color_str, pixel):
+        return self.color_threshold >= self.color_distance(self.colors[color_str], pixel)
+
+    def check_colors(self):
+        c = {color_str: 0 for color_str in self.colors}
+        # sampled_image = self.image[::self.step, ::self.step]
+        sampled_image = self.crop_image()
+        for row in sampled_image:
+            for pixel in row:
+                for color_str in self.colors:
+                    # rospy.loginfo(f"Pixel: {pixel}")
+                    c[color_str] += self.check_1_color(color_str, pixel)*self.step#*self.step
+        m = max(c, key=c.get)
+        self.color = m if c[m] > self.pixel_count_threshold else None # si utilitzem un threshold posat a ma
+
+        rospy.loginfo("")
+        rospy.loginfo(f"{c}")
+        rospy.loginfo("")
+
+    ###################################
+    ### methods for HSV colors#########
+    ###################################
+    def crop_image(self):
+        height, width, channels = self.image.shape
+        descentre = height // 4
+        rows_to_watch = height // 2
+        cropped_image = self.image[(height)//2+descentre:(height), width//5:4*width//5] # podem pillar nomes la meitat amunt o avall tipo el terra o no
+        # si = cropped_image[::self.step, ::self.step]
+        # si = cropped_image[::, ::self.step]
+        rospy.loginfo(f"{self.image.shape} --> {cropped_image.shape}")
+        return cropped_image
+    def count_pixels(self, img):
+        c = {color_str: 0 for color_str in self.colors}
+        for color in self.colors_hsv:
+            c[color] = self.step*self.step*np.sum(cv2.inRange(img, self.colors_hsv[color][0], self.colors_hsv[color][1]))
+        
+        rospy.loginfo("")
+        rospy.loginfo(f"{c}")
+        rospy.loginfo("")
+        
+        return c
+
+
+    def check_colors_hsv(self):
+        
+        sampled_image = self.crop_image()
+        self.hsv = cv2.cvtColor(sampled_image, cv2.COLOR_BGR2HSV)
+        c = self.count_pixels(sampled_image)
+
+        m = max(c, key=c.get)
+        self.color = m if c[m] > self.pixel_count_threshold else None
+
+
+
+    def camera_callback(self, messages):
+        try:
+            self.image = self.bridge_object.imgmsg_to_cv2(messages, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            print(e)
+        
+        # self.check_colors()
+        self.check_colors()
+        if self.color:
+            rospy.loginfo(f"Found color {self.color} OuO") #, shape {self.image.size, self.image.shape}")
+        else:
+            rospy.loginfo("No signal color found")
+        
+        msg = Int8()
+        msg.data = self.map_colors[self.color]
+        self.pub.publish(msg)
+        
+        """if self.color == "red":
+            rospy.loginfo("detected red, closing eyes for 3 seconds")
+            rospy.sleep(10)"""
+        
+if __name__== "__main__":
+    np.random.seed(373)
+    rospy.init_node("color_viewer_node")
+    color_viewer()
+    try:
+        rospy.spin()
+        rospy.Rate(1).sleep()
+    except rospy.ROSInterruptException:
+        print("ROS Interrupt Exception")
